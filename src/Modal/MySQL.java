c3dceb4d5434df9ff89306ec28a1aa0d27b9ea3d
Upek Hansaja <upekhansajabk@gmail.com>
2024-06-30 02:02:18 +0530

    preparing a Student management system

--- src/Modal/MySQL.java
+++ src/Modal/MySQL.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modal;
 
-/**
- *
- * @author upekhansaja
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MySQL {
+
+    private static Connection connection;
+
+    static {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms", "root", "Upek@2004");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static ResultSet execute(String query) throws Exception {
+
+        Statement statement = connection.createStatement();
+
+        if (query.startsWith("SELECT")) {
+            return statement.executeQuery(query);
+        } else {
+            statement.executeUpdate(query);
+            return null;
+        }
+
+    }
     
 }
